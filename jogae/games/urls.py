c28353b52e7d088c1a0674e8f6a07340a5b615de
A-Weine <weine.sousa082@gmail.com>
2025-05-27 14:17:44 -0300

    feat: adicionada rota pra pagina inicial do site

--- jogae/games/urls.py
+++ jogae/games/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 app_name = "games"
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path("<str:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<uuid:pk>/", views.DetailView.as_view(), name="detail"),
 ]
--- jogae/jogae/urls.py
+++ jogae/jogae/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from app_cadastro_usuario import views
+from games.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('cadastro_usuario/', views.registrar_usuario, name='registrar_usuario'),
     path('cadastro_usuario/pagina_sucesso', views.pagina_sucesso, name='pagina_sucesso'),
+    path('', IndexView.as_view(), name='home'),
     path('games/', include("games.urls"))
 ]
