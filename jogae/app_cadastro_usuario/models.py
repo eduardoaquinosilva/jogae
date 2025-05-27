acb8bcc53b1e641a721e8883a36c49fdeb5d1056
Rafael Martinho e Silva Campos Neves <rafaelneves0405@gmail.com>
2025-05-26 22:38:40 -0300

    feat: adiciona id_usuario

--- jogae/app_cadastro_usuario/models.py
+++ jogae/app_cadastro_usuario/models.py
@@ -4,9 +4,7 @@ from django.db import models
 # Create your models here.
 
 class Usuario(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
     username = models.CharField(max_length=150)
     email = models.EmailField()
     password = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.username
