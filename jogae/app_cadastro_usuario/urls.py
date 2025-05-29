f96889ae38c2e65b7e8acf6fc3a7c80b71fa783d
Rafael Araújo <araujorafael9201@gmail.com>
2025-05-29 19:58:59 -0300

    refactor: use django's default auth views

---
+++ jogae/app_cadastro_usuario/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include
+
+app_name="auth"
+urlpatterns = [
+    path('', include("django.contrib.auth.urls")),
+]
--- jogae/jogae/urls.py
+++ jogae/jogae/urls.py
@@ -7,9 +7,7 @@ from games.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
-    path('cadastro_usuario/', views.registrar_usuario, name='registrar_usuario'),
-    path('cadastro_usuario/pagina_sucesso/', views.pagina_sucesso, name='pagina_sucesso'),
-    path('login/', views.login_view, name='login'),
     path('', IndexView.as_view(), name='home'),
-    path('games/', include("games.urls"))
+    path('games/', include("games.urls")),
+    path('auth/', include("app_cadastro_usuario.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
